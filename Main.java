606fcc16c35302549fee9317007577f1bd77a295
Rabunnisa Zabiulla <rabia.zabiulla@gmail.com>
2021-05-20 10:35:42 +0200

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+        public static void main(String[] args){
+		//Create a square using the Point, Line, and Square classes
+		//Print the area and circumreference of the square
+		//Print using the "System.out.println()" command
+		
+        }
+}
---
+++ Rectangle.java
@@ -0,0 +1,19 @@
+public class Rectangle{
+	//Add the fields necessary to create a rectangle here.
+	//Make use of the Line class
+
+
+	//Create a constructor which recieves and stores 4 lines when created
+	public Rectangle(){
+	}
+
+	//Implement a method which returns the circumreference of the rectangle
+	public double circumreference(){
+		return 0;
+	}	
+
+	//Implement a method which returns the area of the rectangle
+	public double area(){
+		return 0;
+	}
+}
